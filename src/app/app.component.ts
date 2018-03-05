f3e7b2ba76f18f226f671166ffc5a2db5800905d
Bohdan <bvakhula@yandex.com>
2018-03-05 13:53:57 +0200

    customize node

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,23 @@ export class AppComponent {
       uid: 'item-2',
       position: {
         left: 200,
-        top: 300
+        top: 400
+      },
+      title: 'Second Item'
+    },
+    {
+      uid: 'item-3',
+      position: {
+        left: 500,
+        top: 400
+      },
+      title: 'Second Item'
+    },
+    {
+      uid: 'item-4',
+      position: {
+        left: 800,
+        top: 400
       },
       title: 'Second Item'
     }
@@ -40,9 +56,40 @@ export class AppComponent {
             jsPlumb.addEndpoint("item-2", { 
               anchor:"Top"
             }, common);
+            
+            jsPlumb.addEndpoint("item-3", { 
+              anchor:"Top"
+            }, common);
+            
+            jsPlumb.addEndpoint("item-4", { 
+              anchor:"Top"
+            }, common);
+
+            jsPlumb.connect({
+                source:"item-1",
+                target:"item-2",
+                anchor: ["Bottom", "Top"],
+                connector: ["Straight"],
+            });
+
+            jsPlumb.connect({
+                source:"item-1",
+                target:"item-3",
+                anchor: ["Bottom", "Top"],
+                connector: ["Straight"],
+            });
+
+            jsPlumb.connect({
+                source:"item-1",
+                target:"item-4",
+                anchor: ["Bottom", "Top"],
+                connector: ["Straight"],
+            });
 
             jsPlumb.draggable('item-1');
             jsPlumb.draggable('item-2');
+            jsPlumb.draggable('item-3');
+            jsPlumb.draggable('item-4');
         });
   }
 }
